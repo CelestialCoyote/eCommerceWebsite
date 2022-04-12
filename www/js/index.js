cea510324241153f9ce69b3cd94b27164c43bce1
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-04-12 17:41:29 -0400

    Added alert box to contact submit form.

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,21 @@
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}; 
+document.getElementById('submit').addEventListener('click', function(event) {
+    // Stop page refresh.
+    event.preventDefault();
+
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let emailAddress = document.getElementById('emailAddress').value;
+    let message = document.getElementById('message').value;
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(emailAddress);
+    console.log(message);
+
+    alert(`${firstName} ${lastName}\n${emailAddress}\n${message}`);
+
+    document.getElementById('firstName').value = '';
+    document.getElementById('lastName').value = '';
+    document.getElementById('emailAddress').value = '';
+    document.getElementById('message').value = '';
+}); 
